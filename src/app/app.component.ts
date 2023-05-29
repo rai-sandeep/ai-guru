9a74bea1cc4c15824a4772926eab7f5b037fe5aa
Sandeep Rai <sandeepksd@gmail.com>
2023-05-29 12:49:34 +0530

    First working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
+// app.component.ts
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface ApiResponse {
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-ui';
+  inputValue: string = '';
+  wisdomText: string = '';
+  isButtonDisabled: boolean = false;
+  isGettingWisdom: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  submitForm() {
+    const url = 'https://jj574jft7k7y7i7dqqai3pe7kq0sfdno.lambda-url.ap-south-1.on.aws';
+    const payload = { payload: this.inputValue };
+
+    this.isButtonDisabled = true; // Disable the button
+    this.isGettingWisdom = true; // Set flag for "GETTING WISDOM..." button text
+
+    this.http.post<ApiResponse>(url, payload)
+      .subscribe(
+        response => {
+          this.wisdomText = response.text.replace(/\n/g, '<br>');
+          this.isButtonDisabled = false; // Re-enable the button
+          this.isGettingWisdom = false; // Reset flag for "GET WISDOM" button text
+        },
+        error => {
+          console.error('An error occurred while making the POST request:', error);
+          this.isButtonDisabled = false; // Re-enable the button even on error
+          this.isGettingWisdom = false; // Reset flag for "GET WISDOM" button text
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
